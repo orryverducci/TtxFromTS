2ac5c8b87459e1cadb1bcf0b8c3c37ac530893b4
Orry Verducci <orry@orryverducci.co.uk>
2018-01-24 20:38:24 +0000

    Initial commit

---
+++ TtxFromTS/Options.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using CommandLineParser.Arguments;
+
+namespace TtxFromTS
+{
+    /// <summary>
+    /// Provides option properties for the application
+    /// </summary>
+    public class Options
+    {
+        /// <summary>
+        /// Gets or sets the input file.
+        /// </summary>
+        /// <value>The input file.</value>
+        [FileArgument('i', "input", Description = "Input TS file to read", Optional = false, FileMustExist = true)]
+        public FileInfo InputFile { get; set; }
+
+        /// <summary>
+        /// Gets or sets the packet identifier.
+        /// </summary>
+        /// <value>The packet identifier.</value>
+        [ValueArgument(typeof(int), "pid", Description = "Packet identifier of the Teletext service to decode", Optional = false)]
+        public int PacketIdentifier { get; set; }
+    }
+}
---
+++ TtxFromTS/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using CommandLineParser.Exceptions;
+
+namespace TtxFromTS
+{
+    /// <summary>
+    /// Provides the main functionality for the application
+    /// </summary>
+    internal class Program
+    {
+        #region Private Fields
+        /// <summary>
+        /// The application options.
+        /// </summary>
+        private static Options _options = new Options();
+        #endregion
+
+        #region Main Application Methods
+        /// <summary>
+        /// The entry point of the application.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        internal static void Main(string[] args)
+        {
+            // Parse command line arguments, and run application if successful
+            if (ParseArguments(args))
+            {
+            }
+        }
+
+        /// <summary>
+        /// Parses and validates the command line arguments.
+        /// </summary>
+        /// <returns><c>true</c> if arguments was successfully parsed, <c>false</c> otherwise.</returns>
+        /// <param name="args">The command line arguments</param>
+        private static bool ParseArguments(string[] args)
+        {
+            // Create parser
+            CommandLineParser.CommandLineParser parser = new CommandLineParser.CommandLineParser();
+            // Set help header
+            parser.ShowUsageHeader = $"TtxFromTS {Assembly.GetEntryAssembly().GetName().Version.ToString(2)}{Environment.NewLine}";
+            // Show help information if no arguments are provided
+            parser.ShowUsageOnEmptyCommandline = true;
+            // Parse command line arguments into the options class
+            parser.ExtractArgumentAttributes(_options);
+            try
+            {
+                parser.ParseCommandLine(args);
+                // Return success
+                return true;
+            }
+            catch (Exception exception)
+            {
+                // Output error messages for argument exceptions, otherwise rethrow exception
+                switch (exception)
+                {
+                    case FileNotFoundException notFoundException: // Input file could not be found
+                        OutputError("ERROR: Input file could not be found");
+                        break;
+                    case MandatoryArgumentNotSetException notSetException: // Required argument not provided
+                        OutputError($"The {notSetException.Argument} argument is required");
+                        break;
+                    case UnknownArgumentException unknownException: // Not a valid argument
+                        OutputError($"{unknownException.Argument} is not a valid argument");
+                        break;
+                    case CommandLineFormatException formatException: // Short argument used with two dashes
+                        OutputError("Short arguments must be prefixed with a single '-' character");
+                        break;
+                    default:
+                        throw exception;
+                }
+                // Return failure
+                return false;
+            }
+        }
+        #endregion
+
+        #region Console Output Methods
+        /// <summary>
+        /// Outputs an error message to the console's standard error output.
+        /// </summary>
+        /// <param name="errorMessage">The error message to be displayed.</param>
+        private static void OutputError(string errorMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.Write("[ERROR] ");
+            Console.ResetColor();
+            Console.Error.WriteLine(errorMessage);
+        }
+        #endregion
+    }
+}
